1b90673f9cb16256244dd1efb23c6e73a5965e39
imagineeeinc <59028351+imagineeeinc@users.noreply.github.com>
2024-12-12 22:30:09 +0000

    added rerouting to spa

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { redirect } from '@sveltejs/kit'
+	import { page } from '$app/stores'
+
+	throw redirect(303, `/#${$page.url.pathname}`)
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,23 @@
 	import Chatbox from '$lib/componets/chatbox.svelte';
 	import P2p from '$lib/componets/p2p.svelte';
 	import Create from '../lib/componets/create.svelte';
+	import ScanQr from '../lib/componets/scanQr.svelte';
+	import { socket } from '$lib/js/backend.js';
+	import { page } from '$app/stores'
 
 	import { Router, Route, navigate } from 'svelte-routing'
+  import { onMount } from 'svelte';
+
+	onMount(() => {
+		if ($page.url.hash) {
+			let interval = setInterval(() => {
+				if (socket.connected) {
+					navigate($page.url.hash.slice(2))
+					clearInterval(interval)
+				}
+			}, 10)
+		}
+	})
 </script>
 
 <Contacts />
@@ -12,5 +27,6 @@
 <Router>
   <Route path="chat/:uid" component={Chatbox} />
 	<Route path="p2p" component={P2p} />
+	<Route path="p2p/scan" component={ScanQr} />
 	<Route path="create" component={Create} />
 </Router>
